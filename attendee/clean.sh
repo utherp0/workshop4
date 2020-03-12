7343ea3f8a6a8c0f83afc5e65332482c293809cd
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2020-03-12 08:08:30 +0000

    Added git clone to Image for workshop4 source, added clean.sh to attendee for deep cleanse of namespace

---
+++ attendee/clean.sh
@@ -0,0 +1,33 @@
+echo "Removing components from current namespace"
+
+for deployment in $(oc get deployments -o jsonpath='{.items[*].metadata.name}');
+do
+  oc delete deployment $deployment;
+done
+
+for deploymentconfig in $(oc get dc -o jsonpath='{.items[*].metadata.name}');
+do
+  oc delete dc $deploymentconfig;
+done
+
+for buildconfig in $(oc get bc -o jsonpath='{.items[*].metadata.name}');
+do
+  oc delete bc $buildconfig;
+done
+
+for service in $(oc get svc -o jsonpath='{.items[*].metadata.name}');
+do
+  oc delete svc $service;
+done
+
+for route in $(oc get route -o jsonpath='{.items[*].metadata.name}');
+do
+  oc delete route $route;
+done
+
+for is in $(oc get is -o jsonpath='{.items[*].metadata.name}');
+do
+  oc delete is $is;
+done
+
+
