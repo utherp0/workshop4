41f37bb3f3f9e61bd409c6dca71f2e9124ae7cc4
Mark Roberts <marrober@redhat.com>
2020-04-15 09:37:03 +0100

    adding ip package

--- attendee/myholiday/adventure/adventure.js
+++ attendee/myholiday/adventure/adventure.js
@@ -1,21 +1,21 @@
 const express = require('express');
-var ip = require("ip");
 const app = express();
-const router = express.Router();
 
-var targetPort = 8080;
 var versionIdentifier = "v1.0";
+const port = 8080;
 
-const port = targetPort;
+var holidayType = process.env.HOLIDAY_TYPE;
 
+var path = "/" + holidayType;
 
-app.get('/adventure', (request, response) => {
-  console.log("adventure holiday - " + versionIdentifier);
-  response.send('This is the adventure holiday application ' + versionIdentifier);
+app.get(path, (request, response) => {
+  console.log("holiday " + holidayType + " url hit .... ");
+  response.send('This is the " + holidayType + " holiday application ' + versionIdentifier);
 
 });
 
 console.log( ip.address() );
+console.log(holidayType + " holiday - " + versionIdentifier);
 
 // set the server to listen on the designated port
 app.listen(port, () => console.log("Listening on port  + ${port}"));
