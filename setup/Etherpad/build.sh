d3a3c8375c19e0148b7cec0fd6978d3f8e7b6244
Ally Kouao <akouao@akouao.fab.csb>
2020-10-27 12:52:22 +0000

    Up-to-date etherpad. note - must populate after deploying

--- setup/Etherpad/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export VERSION=1.7.5
-docker build . -t quay.io/wkulhanek/etherpad:${VERSION}
-docker tag quay.io/wkulhanek/etherpad:${VERSION} quay.io/wkulhanek/etherpad:latest
-docker push quay.io/wkulhanek/etherpad:${VERSION}
-docker push quay.io/wkulhanek/etherpad:latest
-#git tag ${VERSION}
-#git push origin ${VERSION}
--- setup/Etherpad/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-oc new-project etherpad --display-name "Shared Etherpad"
-oc new-app mysql-persistent --param MYSQL_USER=ether --param MYSQL_PASSWORD=ether --param MYSQL_DATABASE=ether --param VOLUME_CAPACITY=4Gi --param MYSQL_VERSION=5.7
-sleep 15
-oc new-app -f etherpad-template.yaml -p DB_USER=ether -p DB_PASS=ether -p DB_DBID=ether -p DB_PORT=3306 -p DB_HOST=mysql -p ADMIN_PASSWORD=secret
